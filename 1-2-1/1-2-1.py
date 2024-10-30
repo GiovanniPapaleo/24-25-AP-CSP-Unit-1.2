8e12d5c9455bacf57860589d8be6c8d48f7b4dfa
Giovanni <giovanni.papaleo@vtsd.com>
2024-10-30 12:15:58 -0400

    day 1/day 2

--- 1-2-1/1-2-1.py
+++ 1-2-1/1-2-1.py
@@ -1,19 +1,39 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
+import random as rand
 
 #-----game configuration----
-spot_color = "pink"
+spot_color = "yellow"
 spot_size = 2
 spot_shape = "circle"
+score = 0
+
 
 #-----initialize turtle-----
 spot = trtl.Turtle()
 spot.shape(spot_shape)
 spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
+spot.pencolor("orange")
+spot.penup()
 
 #-----game functions--------
+def change_position():
+    new_xpos = rand.randint(-400,400)
+    new_ypos = rand.randint(-300,300)
+    spot.goto(new_xpos,new_ypos)
 
+def update_score():
+    global score
+    score += 1
+    print(score)
+
+def spot_clicked (x,y):
+    change_position()
+    update_score()
 
 #-----events----------------
+spot.onclick(spot_clicked)
+wn = trtl.Screen()
+wn.mainloop()
