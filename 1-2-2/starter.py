9448d47fb4ac7a6129167f7b02e6f0778cbb0b3d
Giovanni <giovanni.papaleo@vtsd.com>
2024-11-12 12:26:09 -0500

    1-2-2 day 3/4

--- 1-2-2/starter.py
+++ 1-2-2/starter.py
@@ -8,7 +8,6 @@
 # -----import statements-----
 import turtle as trtl
 import random as rand
-import leaderboard as lb
 
 # -----game configuration-----
 # To view in trinket change the values of font_size, spot_size, and
@@ -18,12 +17,10 @@ font_setup = ("Arial", 20, "normal")
 spot_size = 2
 spot_color = 'pink'
 spot_shape = "turtle"
-timer = 10
+timer = 30
 counter_interval = 1000
 timer_up = False
 score = 0
-leaderboard_file_name =  "a122_leaderboard.txt"
-player_name = input("what is your name?")
 
 # -----initialize the turtles-----
 spot = trtl.Turtle()
@@ -56,7 +53,6 @@ def countdown():
     if timer <= 0:
         counter.write("Time's Up", font=font_setup)
         timer_up = True
-        manage_leaderboard()
     else:
         counter.write("Timer: " + str(timer), font=font_setup)
         timer -= 1
@@ -112,27 +108,6 @@ def change_position():
 def start_game():
     spot.onclick(spot_clicked)
     counter.getscreen().ontimer(countdown, counter_interval)
-
-
-# Add this function to your game code
-
-# manages the leaderboard for top 5 scorers
-def manage_leaderboard():
-
-  global score
-  global spot
-
-  # get the names and scores from the leaderboard file
-  leader_names_list = lb.get_names(leaderboard_file_name)
-  leader_scores_list = lb.get_scores(leaderboard_file_name)
-
-  # show the leaderboard with or without the current player
-  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
-    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
-    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
-
-  else:
-    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
 
 
 # ----------events----------
