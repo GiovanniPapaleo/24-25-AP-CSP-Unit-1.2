db55307652fadef1e3de0df3dd8a3e217c1a0a48
Giovanni <giovanni.papaleo@vtsd.com>
2024-11-13 11:36:40 -0500

    1-2-2 day 5

--- 1-2-2/leaderboard.py
+++ 1-2-2/leaderboard.py
@@ -69,21 +69,23 @@ def get_scores(file_name):
 def update_leaderboard(file_name, leader_names, leader_scores, player_name, player_score):
     index = 0
     # TODO 8: loop through all the scores in the existing leaderboard list
-    '''
+
     for index in range(len(leader_scores)):
       # TODO 9: check if this is the position to insert new score at
       if (player_score >= leader_scores[index]):
         break
       else:
         index = index + 1
-    '''
+
 
     # TODO 10: insert new player and score
-
+    list.insert(index, leader_scores)
     # TODO 11: keep both lists at 5 elements only (top 5 players)
-
+    if (len(leader_names) > 5):
+        leader_names.pop()
+        leader_scores.pop()
     # TODO 12: store the latest leaderboard back in the file
-    '''
+
     leaderboard_file = open(file_name, "w")  # this mode opens the file and erases its contents for a fresh start
 
     # TODO 13 loop through all the leaderboard elements and write them to the the file
@@ -91,7 +93,7 @@ def update_leaderboard(file_name, leader_names, leader_scores, player_name, play
       leaderboard_file.write(leader_names[index] + "," + str(leader_scores[index]) + "\n")
 
     leaderboard_file.close()
-     '''
+
 
 
 # draw leaderboard and display a message to player
