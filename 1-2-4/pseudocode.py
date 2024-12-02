73f88c8451690ab644067c3b9b05c28672c23274
Giovanni <giovanni.papaleo@vtsd.com>
2024-12-02 13:10:02 -0500

    1-2-4

--- 1-2-4/pseudocode.py
+++ 1-2-4/pseudocode.py
@@ -1,30 +1,36 @@
 import turtle as t
 
+# Initialize variables
 num_walls = 20
 path_width = 10
 wall_color = "black"
 wall_length = 20
 barrier_length = (path_width * 2)
 
+# DRAW MAZE
+# Set up turtle
 maze_painter = t.Turtle()
 maze_painter.pencolor(wall_color)
 maze_painter.hideturtle()
-
+# Maze walls
 for num in range(num_walls):
     maze_painter.forward(wall_length)
     maze_painter.left(90)
     wall_length = wall_length + path_width
+    # Draw doors and barriers
     if num in range(num_walls):
+        # Draw doors
         maze_painter.forward(10)
         maze_painter.penup()
         maze_painter.forward(10)
         maze_painter.pendown()
+        # Draw barriers
         maze_painter.forward(40)
         maze_painter.left(90)
         maze_painter.forward(path_width*2)
         maze_painter.back(path_width*2)
         maze_painter.right(90)
 
-
+# Hold display
 wn = t.Screen()
 wn.mainloop()
